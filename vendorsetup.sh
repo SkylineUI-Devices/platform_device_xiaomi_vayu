b5a4f4790105aeced24d2c30a75f84fe5932a961
GXC2356 <gxcdev2356@gmail.com>
2025-01-19 16:57:12 +0200

    vendorsetup: Clone Gcam repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,7 @@ git clone --depth=1 https://github.com/bagaskara815/kernel_xiaomi_vayu.git -b 14
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-22.0
 rm -rf hardware/xiaomi/megvii
+
+# Gcam
+rm -rf vendor/mgc
+git clone https://bitbucket.org/vendor-mgc/vendor_mgc.git vendor/mgc
